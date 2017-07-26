6e2ea0860c970fcf57365c6f702ae34367fc77db
sabarni.d <sabarni.d@media.net>
2017-07-26 21:02:02 +0530

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+   entry: path.resolve(__dirname, 'resources/js/index.js'),
+   output: {
+     filename: 'public/bundle.js'
+   },
+   module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [ 'style-loader', 'css-loader' ]
+      }
+    ]
+  },
+  plugins: [
+          new webpack.ProvidePlugin({
+              "React": "react",
+          }),
+    ],
+
+}
